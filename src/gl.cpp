9f5784d7223ac3130835cecedbe680884d616fac
Carlos Adriano <cadrianogamac@gmail.com>
2024-11-09 22:25:12 -0300

    Add top-left rule for triangle rasterization

--- src/gl.cpp
+++ src/gl.cpp
@@ -1,4 +1,12 @@
+#include <geometry.hpp>
 #include <gl.hpp>
+
+// Top-left rasterization rule
+bool is_top_left(Vec2i edge) {
+  bool is_top_edge = edge.value.y == 0 && edge.value.x > 0;
+  bool is_left_edge = edge.value.y < 0;
+  return is_top_edge || is_left_edge;
+}
 
 void triangle(Vec2i v0, Vec2i v1, Vec2i v2, TGAImage &image, TGAColor color) {
   // Finding the bounding box
@@ -7,14 +15,18 @@ void triangle(Vec2i v0, Vec2i v1, Vec2i v2, TGAImage &image, TGAColor color) {
   int x_max = std::max(std::max(v0.value.x, v1.value.x), v2.value.x);
   int y_max = std::max(std::max(v0.value.y, v1.value.y), v2.value.y);
   
+  int bias0 = is_top_left(v1 - v0) ? 0 : -1;
+  int bias1 = is_top_left(v2 - v1) ? 0 : -1;
+  int bias2 = is_top_left(v0 - v2) ? 0 : -1;
+  
   for (int y = y_min; y <= y_max; y++) {
     for (int x = x_min; x <= x_max; x++) {
       Vec2i p(x, y);
       
       // Barycentric Weights
-      int w0 = (v1 - v0) ^ (p - v0);
-      int w1 = (v2 - v1) ^ (p - v1);
-      int w2 = (v0 - v2) ^ (p - v2);
+      int w0 = ((v1 - v0) ^ (p - v0)) + bias0;
+      int w1 = ((v2 - v1) ^ (p - v1)) + bias1;
+      int w2 = ((v0 - v2) ^ (p - v2)) + bias2;
       
       if (w0 < 0 || w1 < 0 || w2 < 0) continue;
       image.set(x, y, color);
